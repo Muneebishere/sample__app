2cc3470ff620a0763d5c287c7b050cfaf508febd
Muneeb <muneeb.ahmadb@gmail.com>
2017-08-23 13:55:37 +0500

    signin and signout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      #valid credentials
+      sign_in user
+      redirect_to user
+    else
+      #invalid credentials provided
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
 
   root to: 'static_pages#home', :via => [:get]
@@ -9,6 +9,9 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', :via => [:get]
   match '/about', to: 'static_pages#about', :via => [:get]
   match '/contact', to: 'static_pages#contact', :via => [:get]
+
+  match '/signin', to: 'sessions#new', :via => [:get, :post]
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
